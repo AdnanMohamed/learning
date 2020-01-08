c66a202ac6428bf8916dfa591a2c675e35f3617e
Adnan Mohamed <amohame6@kent.edu>
2020-01-07 20:48:36 -0500

    I dont know what to say

---
+++ Sum of even/SumOfEven.cpp
@@ -0,0 +1,25 @@
+// This change is from another-branch
+
+#include<iostream>
+
+using std::cout; using std::cin; using std::endl;
+
+int main()
+{
+	int Number, Sum=0;
+
+	cout << "Enter the list of even number and if it's finished enter an odd number\n";
+	cin >> Number;
+
+	while (Number % 2 == 0) {
+
+
+		Sum = Sum + Number;
+
+		cin >> Number;
+	}
+
+	cout << "The sum of the list of even numbers= " << Sum << endl;
+	cout << "This change is from master branch" <<endl;
+
+}
