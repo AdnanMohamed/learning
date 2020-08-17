47d394a53816a0921881dd07b56c9f1ead04d856
Adnan Mohamed <amohame6@kent.edu>
2020-08-17 08:14:07 +0300

    second commit

--- test1.py
+++ test1.py
@@ -1,3 +1,5 @@
 # this is written at 8/17/2020 -- 8:07 AM  @A.M-17-8-2020-8:07
 
-print("Hello World!)  @A.M-17-8-2020-8:07
+print("Hello World!")  @A.M-17-8-2020-8:10
+
+print("Good Morning")  @A.M-17-8-2020-8:10
