d7aafb15eac02cae3c72eebe6e08ab68b3cb4a16
Adnan Mohamed <amohame6@kent.edu>
2020-08-17 11:25:28 +0300

    first commit

--- test1.py
+++ test1.py
@@ -1,5 +1,7 @@
-# this is written at 8/17/2020 -- 8:07 AM  @A.M-17-8-2020-8:07
+# this is written at 8/17/2020 -- 8:17 AM  @A.M-17-8-2020-8:17
 
 print("Hello World!")  @A.M-17-8-2020-8:10
 
 print("Good Morning")  @A.M-17-8-2020-8:10
+
+print("Keep LEARNING!!") @A.M-17-8-2020-8:17
